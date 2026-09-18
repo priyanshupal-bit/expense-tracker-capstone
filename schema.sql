6508b065035e5e7aa3d3a8f0c60ba4f6c31fc453
priyanshupal-bit <palpriyanshu7880@gmail.com>
2026-09-19 00:08:42 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS expenses (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    date DATE NOT NULL,
+    category VARCHAR(100) NOT NULL
+);
